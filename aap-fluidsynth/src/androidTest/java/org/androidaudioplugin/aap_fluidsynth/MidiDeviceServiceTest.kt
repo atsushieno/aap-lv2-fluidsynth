0321829b5c2b128ba84ba04e31124e824575e13d
atsushieno <atsushieno@gmail.com>
2022-02-11 18:27:34 +0900

    import MidiDeviceServiceTest.kt from aap-ayumi. I guess I forgot to add it.

---
+++ aap-fluidsynth/src/androidTest/java/org/androidaudioplugin/aap_fluidsynth/MidiDeviceServiceTest.kt
@@ -0,0 +1,30 @@
+package org.androidaudioplugin.aap_fluidsynth
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.rule.ServiceTestRule
+import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginMidiDeviceServiceTesting
+import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginServiceTesting
+import org.junit.Rule
+import org.junit.Test
+
+class MidiDeviceServiceTest {
+
+    private val applicationContext = ApplicationProvider.getApplicationContext<Context>()
+    private val testing = AudioPluginMidiDeviceServiceTesting(applicationContext)
+    @get:Rule
+    val serviceRule = ServiceTestRule()
+
+    @Test
+    fun basicServiceOperations() {
+        // Replace plugin name after copy-pasting this code.
+        testing.basicServiceOperations("aap-fluidsynth")
+    }
+
+    @Test
+    fun repeatDirectServieOperations() {
+        for (i in 0 until 5)
+            basicServiceOperations()
+    }
+
+}
