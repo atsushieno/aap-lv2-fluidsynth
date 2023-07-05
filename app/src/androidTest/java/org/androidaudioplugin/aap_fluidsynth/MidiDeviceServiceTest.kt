3682f29dff6bcbc8431bd6e3ef7cf86f549e3c84
Atsushi Eno <atsushieno@gmail.com>
2023-07-05 20:17:21 +0900

    modernize project to the latest aap-lv2 structure.
    - bump aap-lv2
    - use Compose native UI
    - AGP/Gradle migration with assistant.
    - CMake build is updated to resolve deps as in Prefab manner.

--- app/src/androidTest/java/org/androidaudioplugin/aap_fluidsynth/MidiDeviceServiceTest.kt
+++ app/src/androidTest/java/org/androidaudioplugin/aap_fluidsynth/MidiDeviceServiceTest.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.rule.ServiceTestRule
 import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginMidiDeviceServiceTesting
+import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginServiceTesting
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,7 +18,7 @@ class MidiDeviceServiceTest {
     @Test
     fun basicServiceOperations() {
         // Replace plugin name after copy-pasting this code.
-        testing.basicServiceOperations("aap-fluidsynth")
+        testing.basicServiceOperations("MDA")
     }
 
     @Test
